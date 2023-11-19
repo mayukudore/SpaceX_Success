e99ab6c3e3527fa3d98eec65570c9178808c4400
Dore201 <cirrhosis06@gmail.com>
2023-11-19 14:36:13 -0500

    Creat dash board for better view

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # The code is importing various libraries and modules in Python.
-import pandas as pd
+import numpy as np
 import pandas as pd
 import flask
 import dash
@@ -12,13 +12,20 @@ URL = ('https://raw.githubusercontent.com/mayukudore/SpaceX_Success/main/spacex_
 spacex_df = pd.read_csv(URL)
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
+temp_payload = spacex_df['Payload Mass (kg)'].apply(np.floor).copy()
+index_payload_dict = []
+for work in temp_payload:
+    index_payload_dict.append(str(work))
+
+temp_payload = temp_payload.set_axis(index_payload_dict)
+payload_as_dict = temp_payload.to_dict()
 
 # Create a dash application
 app = dash.Dash(__name__)
 
 # Create an app layout
 app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
-                                        style={'textAlign': 'center', 'color': '#503D36',
+                                        style={'textAlign': 'center', 'color': 'green',
                                                'font-size': 40}),
                                 # TASK 1: Add a dropdown list to enable Launch Site selection
                                 # The default select value is for ALL sites
@@ -35,7 +42,7 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                 html.P("Payload range (Kg):"),
                                 
                                 # TASK 3: Add a slider to select payload range
-                                dcc.RangeSlider(id='payload-slider', min=min_payload, max=max_payload, value=[spacex_df['Payload Mass (kg)']]),
+                                dcc.RangeSlider(id='payload-slider', min=min_payload, max=max_payload, step=None, marks=payload_as_dict, value=[min_payload, max_payload]),
 
                                 # TASK 4: Add a scatter chart to show the correlation between payload and launch success
                                 html.Div(dcc.Graph(id='success-payload-scatter-chart')),
@@ -55,7 +62,19 @@ def get_pie_chart(entered_site):
             chosen_site= chosen_site.to_frame()
             fig = px.pie(values=[chosen_site['class'][1], chosen_site['class'][0]], names = ['Success', 'Failure'])
             return fig
-            
+        
+@app.callback(
+              Output(component_id='success-payload-scatter-chart', component_property='figure'),
+              [Input(component_id='payload-slider', component_property='value'), Input(component_id="site-dropdown", component_property="value")])
+                 
+def get_scatter_graph(payload_slider, site_dropdown):
+    working_df = spacex_df[['Payload Mass (kg)', 'class', 'Booster Version Category']].copy()
+    if payload_slider == [min_payload, max_payload]:
+        fig = px.scatter(data_frame= working_df, x = 'Payload Mass (kg)', y = 'class', color="Booster Version Category")
+        return fig
+    
+  
+       
         
    
 
